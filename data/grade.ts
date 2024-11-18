f6989aba22cd1cdc6ad3a78e52ddb487cb36c49d
HaekyungSeong <haekyungs95@gmail.com>
2024-11-18 10:16:37 +0900

    점수산정 수정

--- data/grade.ts
+++ data/grade.ts
@@ -1,8 +1,8 @@
 export const grade = (score: number) => {
-  if (score === 11) {
+  if (score === 23) {
     return 100;
   } else {
-    const userGrade = Math.floor(score * (100 / 11));
+    const userGrade = Math.floor(score * (100 / 23));
     return userGrade;
   }
 };
