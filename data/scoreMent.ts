99e968c0817a493e4cefc276e327e65a098633ca
HaekyungSeong <haekyungs95@gmail.com>
2024-11-19 10:59:59 +0900

    성적 멘트 변경

--- data/scoreMent.ts
+++ data/scoreMent.ts
@@ -2,11 +2,9 @@
 export const scoreMent = (score: number) => {
   if (score === 0) {
     return `"넌 그냥 나가라"`;
-  } else if (score > 5 && score < 10) {
+  } else if (score > 5 && score < 15) {
     return `"오잉? 똑바로 안해?!"`;
-  } else if (score >= 10 && score < 20) {
-    return `"마이? "`;
-  } else if (score >= 20 && score < 23) {
+  } else if (score >= 15 && score < 23) {
     return `"오오~ 조은데요~"`;
   } else if (score === 23) {
     return `"너 없으면 나 망해"`;
