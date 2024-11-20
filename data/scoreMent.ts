aa3d08e45a147ab2f0c6493a49828c4c8182200b
HaekyungSeong <haekyungs95@gmail.com>
2024-11-20 23:08:08 +0900

    코멘트 수정

--- data/scoreMent.ts
+++ data/scoreMent.ts
@@ -2,9 +2,9 @@
 export const scoreMent = (score: number) => {
   if (score === 0) {
     return `"넌 그냥 나가라"`;
-  } else if (score > 5 && score < 15) {
+  } else if (score > 7 && score < 17) {
     return `"오잉? 똑바로 안해?!"`;
-  } else if (score >= 15 && score < 23) {
+  } else if (score >= 17 && score < 23) {
     return `"오오~ 조은데요~"`;
   } else if (score === 23) {
     return `"너 없으면 나 망해"`;
