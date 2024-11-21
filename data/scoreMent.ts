651be5edae6635cca14949f190e1377fef232a6b
HaekyungSeong <haekyungs95@gmail.com>
2024-11-21 13:59:32 +0900

    멘트 구간 일부없는것 수정

--- data/scoreMent.ts
+++ data/scoreMent.ts
@@ -2,7 +2,9 @@
 export const scoreMent = (score: number) => {
   if (score === 0) {
     return `"넌 그냥 나가라"`;
-  } else if (score > 7 && score < 17) {
+  } else if (score > 1 && score < 8) {
+    return `"와 이것밖에 모른다고?`;
+  } else if (score >= 8 && score < 17) {
     return `"오잉? 똑바로 안해?!"`;
   } else if (score >= 17 && score < 23) {
     return `"오오~ 조은데요~"`;
